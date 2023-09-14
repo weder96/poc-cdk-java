e94a10aa109ac54998b18c58aa12fc7919d9ebcf
weder96 <weder96@gmail.com>
2023-09-13 23:18:33 -0300

    :rocket: delete item

--- my-cdk/lambda/deleteItem.py
+++ my-cdk/lambda/deleteItem.py
@@ -6,25 +6,39 @@ from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
     print('Starting input lambda function call')
-    print(event)
-
     print(event['pathParameters']['id'])
 
     idParam = event['pathParameters']['id']
-
     client = boto3.client('dynamodb')
-    item = client.delete_item( TableName='orders' ,Key={'orderId': { 'S' : str(idParam)}})
 
-    print("The query returned the following item:")
-    print(item)
-
-    response = {
-        'statusCode': 200,
-        'body': 'Item delete with Successfully :'+ str(json.dumps(item)),
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+    item = client.query(
+        ExpressionAttributeValues={
+            ':vId': {
+                'S': str(idParam)
+            },
         },
-    }
+        KeyConditionExpression='orderId = :vId',
+        TableName='orders'
+    )
 
-    return response
+    if len(item["Items"]) > 0:
+        print(item["Items"][0]["orderDate"]["S"])
+        deleteItem = client.delete_item(
+            TableName='orders' ,
+            Key={
+                'orderId': { 'S' : str(idParam)},
+                'orderDate': { 'S' : str(item["Items"][0]["orderDate"]["S"]) }
+            }
+        )
+
+        print("The query returned the following item:")
+        print(deleteItem)
+        response = {
+            'statusCode': 200,
+            'body': 'Item delete with Successfully :'+ str(json.dumps(deleteItem)),
+            'headers': { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+        }
+
+        return response
+    else:
+        raise Exception(str({"message": "orderId Not Found "+ str(idParam)}))
